33966fd24b7310137c97af231cfbd58a2803ffa2
kncode86 <kulcsar.norbert.86@gmail.com>
2025-01-22 21:33:19 +0100

    feat: add background-picture to Header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -56,6 +56,22 @@ const Header = () => {
                             </li>
                         </ul>
                     </nav>
+                    <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+                        <img 
+                            src="/images/bg-outlines.svg" 
+                            alt="outline" 
+                            width={960} 
+                            height={380} 
+                            className="relative z-2"
+                        />
+                        <img 
+                            src="/images/bg-outlines-fill.png" 
+                            alt="outline" 
+                            width={960} 
+                            height={380} 
+                            className="absolute inset-0 mix-blend-soft-light opacity-5"
+                        />
+                    </div>
                 </div>
             </div>
             <button 
