4cbf353c89fd239fe961b8416bcfff2c71509fc3
kncode86 <kulcsar.norbert.86@gmail.com>
2025-01-29 16:54:07 +0100

    feat: create Download-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Download from "./sections/Download";
 import Faq from "./sections/Faq";
 import Features from "./sections/Features";
 import Header from "./sections/Header";
@@ -14,6 +15,7 @@ export default function App() {
       <Pricing />
       <Faq />
       <Testimonials />
+      <Download />
     </main>
   )
 }
